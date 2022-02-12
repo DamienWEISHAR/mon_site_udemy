8d04c2e27bd5f581f5dbd7dbf357bfd35d0fc1f6
Damien WEISHAR <88081429+DamienWEISHAR@users.noreply.github.com>
2022-02-12 17:17:55 +0100

    Update LivresController.controller.php

--- controllers/LivresController.controller.php
+++ controllers/LivresController.controller.php
@@ -37,6 +37,50 @@ class LivresController{
 
     }
 
+    private function ajoutImage($file, $dir){
+        if(!isset($file['name']) || empty($file['name'])){
+            throw new Exception("Vous devez indiquer une image!");
+        }
+        if(!file_exists($dir)) mkdir($dir, 0777);
+
+        //verification de l'extension
+        $extension =strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+        //pour créer un chiffre random entre 0 et 99999
+        $random = rand(0,99999);
+        /*donner un nouveau nom avec le répertoire et un chiffre random
+        permet d'avoir plusieurs fois le même fichier image mais de ne pas écraser l'autre
+        et de pouvoir l'utiliser à plusieurs endroits différents */
+        $target_file = $dir.$random."_".$file['name'];
+
+        /*tests de vérification */
+
+        //est-ce une image?
+        if(!getimagesize($file["tmp_name"])){
+            throw new Exception("Ceci n'est pas une image!");
+        }
+        //bonne extension?
+        if($extension !== "jpg" && $extension !== "jpeg" && $extension !== "png" && $extension !== "gif" ){
+            throw new Exception("L'extension du fichier n'est pas reconnu!");
+        }
+        //déjà existant?
+        if(file_exists($target_file)){
+            throw new Exception("Le fichier existe déjà!");
+        }
+        //fichier trop lourd?
+        if($file['size'] > 50000){
+            throw new Exception("Fichier trop volumineux!");
+        }
+        //fichier déplacé?
+        if(!move_uploaded_file($file['tmp_name'], $target_file)){
+            throw new Exception("L'ajout de l'image n'a pas fonctionné!");
+        }
+        //sinon, retourne le nom du fichier (renommé avec la concaténation)
+        else{
+            return ($random."_".$file['name']);
+        }
+
+    }
+
 
 }
 
